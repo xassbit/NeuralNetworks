561990b7468ac42e562f0df57d333b0b766d3b10
cornelis-vl <cornelisvletter@gmail.com>
2017-02-28 11:58:05 +0100

    Changed setup

--- neural_network.py
+++ neural_network.py
@@ -9,6 +9,8 @@ import math as mt
 import random
 import pickle
 import gzip
+
+from network import Network
 # Functions
 
 # MNIST data
@@ -113,85 +115,7 @@ def one_hot_encoder(vector):
 
 # Neural Network
 
-class Network(object):
 
-    def __init__(self, sizes):
-        self.num_layers = len(sizes)
-        self.sizes = sizes
-        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
-        self.weights = [np.random.randn(y, x)
-                        for x, y in zip(sizes[:-1], sizes[1:])]
-
-    def feedforward(self, a):
-        for b, w in zip(self.biases, self.weights):
-            a = sigmoid(np.dot(w, a) + b)
-            return a
-
-    def SGD(self, training_data, epochs, mini_batch_size, eta,
-            test_data=None):
-
-        if test_data: n_test = len(test_data)
-        n = len(training_data)
-
-        for j in xrange(epochs):
-            random.shuffle(training_data)
-            mini_batches = [
-                training_data[k:k + mini_batch_size]
-                for k in xrange(0, n, mini_batch_size)]
-            for mini_batch in mini_batches:
-                self.update_mini_batch(mini_batch, eta)
-            if test_data:
-                print("Epoch {0}: {1} / {2}").format(
-                    j, self.evaluate(test_data), n_test)
-            else:
-                print("Epoch {0} complete").format(j)
-
-    def update_mini_batch(self, mini_batch, eta):
-        nabla_b = [np.zeros(b.shape) for b in self.biases]
-        nabla_w = [np.zeros(w.shape) for w in self.weights]
-        for x, y in mini_batch:
-            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
-            nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
-            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-        self.weights = [w - (eta / len(mini_batch)) * nw
-                        for w, nw in zip(self.weights, nabla_w)]
-        self.biases = [b - (eta / len(mini_batch)) * nb
-                       for b, nb in zip(self.biases, nabla_b)]
-
-    def backprop(self, x, y):
-        nabla_b = [np.zeros(b.shape) for b in self.biases]
-        nabla_w = [np.zeros(w.shape) for w in self.weights]
-
-        # feed forward
-        activation = x
-        activations = [x]  # list to store all the activations, layer by layer
-        zs = []  # list to store all the z vectors, layer by layer
-        for b, w in zip(self.biases, self.weights):
-            z = np.dot(w, activation) + b
-            zs.append(z)
-            activation = sigmoid(z)
-            activations.append(activation)
-
-        # backward pass
-        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
-        nabla_b[-1] = delta
-        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
-
-        for l in xrange(2, self.num_layers):
-            z = zs[-l]
-            sp = sigmoid_prime(z)
-            delta = np.dot(self.weights[-l + 1].transpose(), delta) * sp
-            nabla_b[-l] = delta
-            nabla_w[-l] = np.dot(delta, activations[-l - 1].transpose())
-        return (nabla_b, nabla_w)
-
-    def evaluate(self, test_data):
-        test_results = [(np.argmax(self.feedforward(x)), y)
-                        for (x, y) in test_data]
-        return sum(int(x == y) for (x, y) in test_results)
-
-    def cost_derivative(self, output_activations, y):
-        return output_activations - y
 
 
 
@@ -204,40 +128,40 @@ def sigmoid_prime(z):
 
 
     
+if __name__ == "__main__":
+    # Script
 
-# Script
-
-work_dir = "/Users/cornelisvletter/Desktop/Programming/NeuralNetworks"
-filename = "/iris.csv"
-file_loc = work_dir + filename
-os.chdir(work_dir)
+    work_dir = "/Users/cornelisvletter/Desktop/Programming/NeuralNetworks"
+    filename = "/iris.csv"
+    file_loc = work_dir + filename
+    os.chdir(work_dir)
 
 
-"""
-#Alternative Dataset
-raw_data, id_col = import_data(file_loc=file_loc,
-                               separator=",",
-                               id_colname="Id"
-                               )
+    """
+    #Alternative Dataset
+    raw_data, id_col = import_data(file_loc=file_loc,
+                                   separator=",",
+                                   id_colname="Id"
+                                   )
 
-raw_features = raw_data.iloc[:, 0:4]
-raw_target = categorical_to_numeric(categorical_data=raw_data["Species"])
+    raw_features = raw_data.iloc[:, 0:4]
+    raw_target = categorical_to_numeric(categorical_data=raw_data["Species"])
 
-y_train_1d, x_train, y_test_1d, x_test = train_test_splitter(target=raw_target,
-                                                       features=raw_features,
-                                                       id_column=id_col,
-                                                       train_size=0.7
-                                                       )
+    y_train_1d, x_train, y_test_1d, x_test = train_test_splitter(target=raw_target,
+                                                           features=raw_features,
+                                                           id_column=id_col,
+                                                           train_size=0.7
+                                                           )
 
-y_train = one_hot_encoder(vector=y_train_1d)                                                        
-y_test = one_hot_encoder(vector=y_test_1d)                                                
-"""
+    y_train = one_hot_encoder(vector=y_train_1d)
+    y_test = one_hot_encoder(vector=y_test_1d)
+    """
 
-#MNIST Data
-training_data, validation_data, test_data = load_data_wrapper()
+    #MNIST Data
+    training_data, validation_data, test_data = load_data_wrapper()
 
-net = Network([784, 30, 10])
-net.SGD(training_data, 3, 10, 3.0, test_data=test_data)
+    net = Network([784, 30, 10])
+    net.SGD(training_data, 3, 10, 3.0, test_data=test_data)
 
-weights = net.weights
-biases = net.biases
+    weights = net.weights
+    biases = net.biases
